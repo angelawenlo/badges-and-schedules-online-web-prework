12f6c5527766050168e262088960b3712504a562
Angela Lo <angelawenlo@gmail.com>
2019-05-29 18:54:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |person|
     puts person
   end
-  assign_rooms(attendees).eacg do |person|
+  assign_rooms(attendees).each do |person|
     puts person
   end
 end
